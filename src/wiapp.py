b56bce4d4d468c119a7d6c52f59c0ced61bb788e
Faridsz0605 <farid.s.villa@gmail.com>
2026-03-16 16:27:31 -0500

    base docs

--- src/wiapp.py
+++ src/wiapp.py
@@ -1,9 +1,18 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 
 app = FastAPI()
 
+text_posts = {1: {"hello": "world", "content": "example"}}
+
 
 # define methods
-@app.get("/hello-world")
-def hello_world():
-    return {"message": "Hello World"}
+@app.get("/posts ")
+def get_posts():
+    return text_posts
+
+
+@app.get(f"/posts/{id}")
+def get_single_post(id: int):
+    if id not in text_posts:
+        raise HTTPException(status_code=404, detail="no such post/directory")
+    return text_posts.get(id)
