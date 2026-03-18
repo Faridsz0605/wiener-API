4635438c4e29673df9129b276b3446599b946d6a
Faridsz0605 <farid.s.villa@gmail.com>
2026-03-18 15:39:02 -0500

    DB implementation for local conection

--- src/wiapp.py
+++ src/wiapp.py
@@ -1,19 +1,19 @@
+from contextlib import asynccontextmanager
+
 from fastapi import FastAPI, HTTPException
+from sqlalchemy.ext.asyncio import AsyncSession
 
-app = FastAPI()
+from src.DB import Post, get_async_session, init_db
+from src.schema import Post, PostResponse
 
-text_posts = {
-    1: {"hello": "world", "content": "example"},
-    2: {"hello": "universe", "content": "exploration"},
-    3: {"hello": "dev", "content": "debugging"},
-    4: {"hello": "user", "content": "profile_data"},
-    5: {"hello": "client", "content": "request_payload"},
-    6: {"hello": "server", "content": "response_status"},
-    7: {"hello": "cloud", "content": "deployment"},
-    8: {"hello": "coder", "content": "syntax_highlight"},
-    9: {"hello": "tester", "content": "unit_tests"},
-    10: {"hello": "admin", "content": "dashboard_stats"},
-}
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    await init_db()
+    yield
+
+
+app = FastAPI(lifespan=lifespan)
 
 
 # define methods
@@ -29,3 +29,14 @@ def get_single_post(id: int):
     if id not in text_posts:
         raise HTTPException(status_code=404, detail="no such post/directory")
     return text_posts.get(id)
+
+
+@app.post("/posts")
+def create_post(post: Post) -> PostResponse:
+    new_post = {"title": post.title, "content": post.content}
+
+    text_posts[max(text_posts.keys()) + 1] = {
+        "title": post.title,
+        "content": post.content,
+    }
+    return new_post
